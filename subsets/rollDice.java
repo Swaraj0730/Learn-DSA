bcf401100a95c65c653ca4a3b90c56cf0541163f
swaraj namdeo <swarajnamdeo@swarajs-MacBook-Air.local>
2026-08-17 02:43:08 +0530

    new

---
+++ subsets/rollDice.java
@@ -0,0 +1,66 @@
+package subsets;
+
+import java.util.ArrayList;
+
+public class rollDice {
+
+    public static void roll(String p , int target){
+        if(target == 0 ){
+            System.out.println(p);
+            return ;
+        }
+
+        for(int i = 1 ; i <= 6 && i <= target ; i++){
+            roll( p + i , target - i);
+        }
+    }
+
+    public static ArrayList<String> rollList(String p , int target){
+        if(target == 0 ){
+            ArrayList<String> str = new ArrayList<>();
+            str.add(p);
+            return str;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 1 ; i <= 6 && i <= target ; i++){
+            list.addAll(rollList( p + i , target - i));
+        }
+        return list ; 
+    }
+
+    public static void rollwithFace(String p , int target , int face){
+        if(target == 0 ){
+            System.out.println(p);
+            return ;
+        }
+
+        for(int i = 1 ; i <= face && i <= target ; i++){
+            rollwithFace(p + i , target - i , face);
+        }
+    }
+
+    public static int rollwithFace2(int n , int target , int face){
+
+        if(target == 0 ){
+            return 1 ;
+        }
+        
+        int count = 0 ;
+        for(int i = 1 ; i <= face && i <= target ; i++){
+            count = count + rollwithFace2(n + i , target - i , face);
+        }
+        return count ;
+    }
+    
+
+    public static void main(String[] args) {
+
+        // roll("", 4);
+        // System.out.println(rollList("", 4));
+        // rollwithFace("", 4, 7);
+
+        System.out.println(rollwithFace2(2, 3, 6));
+        
+    }
+    
+}
